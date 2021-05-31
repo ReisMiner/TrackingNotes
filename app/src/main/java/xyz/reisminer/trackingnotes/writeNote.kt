39fff366fab9e8d552d6527e7f94b1a6fc13e3d5
jordan.dainese <jordan.dainese@siemens.com>
2021-05-31 15:27:01 +0200

    second activity to edit notes

---
+++ app/src/main/java/xyz/reisminer/trackingnotes/writeNote.kt
@@ -0,0 +1,11 @@
+package xyz.reisminer.trackingnotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class writeNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_write_note)
+    }
+}
