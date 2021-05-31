e6f0c9d8e10fabc50b6f342ae6e9fa9982170094
ReisMiner <r1miner@outlook.com>
2021-05-31 22:51:51 +0200

    some stuff idk what

--- app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
+++ app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
@@ -1,15 +1,47 @@
 package xyz.reisminer.trackingnotes
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.app.ActionBar
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TableLayout
+import android.widget.TableRow
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setMargins
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, writeNote::class.java)
-        // start your next activity
-        startActivity(intent)
+
+        //dynamic fields dings
+        val table = findViewById<TableLayout>(R.id.mainTable)
+
+        val params = TableLayout.LayoutParams(
+            TableRow.LayoutParams.MATCH_PARENT,
+            TableRow.LayoutParams.MATCH_PARENT
+        )
+        params.setMargins(10,10,10,10)
+
+        val tr = TableRow(this)
+        tr.layoutParams = params
+
+        val o = View.inflate(this,R.layout.tiles_home,null)
+        val o2 = View.inflate(this,R.layout.tiles_home,null)
+        val o3 = View.inflate(this,R.layout.tiles_home,null)
+
+        tr.addView(o)
+        tr.addView(o2)
+
+        table.addView(
+            tr,
+            params
+        )
+        //dynamic field dings fertig
+
+//        val intent = Intent(this, writeNote::class.java)
+//        startActivity(intent)
     }
+
+
 }
