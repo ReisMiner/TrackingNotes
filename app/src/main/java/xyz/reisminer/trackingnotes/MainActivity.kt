6b650fa9d6a2ef4459f80f7e46dc52793e5989cd
ReisMiner <r1miner@outlook.com>
2021-06-01 11:03:19 +0200

    layout finished

--- app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
+++ app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
@@ -1,15 +1,9 @@
 package xyz.reisminer.trackingnotes
 
 import android.content.Intent
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
-import android.view.View
-import android.view.ViewParent
-import android.view.Window
 import android.widget.TableLayout
 import android.widget.TableRow
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
 
