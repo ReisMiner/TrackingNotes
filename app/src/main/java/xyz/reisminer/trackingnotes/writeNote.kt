17fd561ae655f0ddf3f215536686edb740525f27
jordan.dainese <jordan.dainese@siemens.com>
2021-06-01 11:56:19 +0200

    start of logik in write activity

--- app/src/main/java/xyz/reisminer/trackingnotes/writeNote.kt
+++ app/src/main/java/xyz/reisminer/trackingnotes/writeNote.kt
@@ -1,11 +1,26 @@
 package xyz.reisminer.trackingnotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
 
 class writeNote : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_write_note)
+        val createButton = findViewById<AppCompatButton>(R.id.createButton)
+        createButton.setOnClickListener{
+            saveNote()
+        }
+    }
+
+    fun saveNote (){
+        val titleEditView = findViewById(R.id.TextViewSaveTitle) as EditText
+        val contentEditView = findViewById(R.id.TextViewSaveText) as EditText
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
