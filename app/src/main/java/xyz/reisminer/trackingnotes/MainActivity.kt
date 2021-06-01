27a563914209c48af490161b4c77881cef3e6973
ReisMiner <r1miner@outlook.com>
2021-06-01 10:54:10 +0200

    layout finished

--- app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
+++ app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
@@ -1,18 +1,30 @@
 package xyz.reisminer.trackingnotes
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.View
+import android.view.ViewParent
+import android.view.Window
 import android.widget.TableLayout
 import android.widget.TableRow
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.activity_main)
 
-        createHomeTiles(1000)
+        createHomeTiles(10)
+
+        setContentView(R.layout.titlebar);
+        window.setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.titlebar);
+
 
         val createButton = findViewById<AppCompatButton>(R.id.createButton)
         createButton.setOnClickListener {
