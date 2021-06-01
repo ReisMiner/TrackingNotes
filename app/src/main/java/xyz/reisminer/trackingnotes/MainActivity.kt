15da4f0b4545a93d08242bf5bb4c8e4b027ace8b
ReisMiner <r1miner@outlook.com>
2021-06-01 08:59:19 +0200

    create tiles dynamically

--- app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
+++ app/src/main/java/xyz/reisminer/trackingnotes/MainActivity.kt
@@ -1,47 +1,59 @@
 package xyz.reisminer.trackingnotes
 
-import android.app.ActionBar
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import android.widget.TableLayout
 import android.widget.TableRow
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.setMargins
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //dynamic fields dings
+        createHomeTiles(1000)
+
+//        val intent = Intent(this, writeNote::class.java)
+//        startActivity(intent)
+    }
+
+    private fun createHomeTiles(y:Int){
         val table = findViewById<TableLayout>(R.id.mainTable)
 
         val params = TableLayout.LayoutParams(
             TableRow.LayoutParams.MATCH_PARENT,
             TableRow.LayoutParams.MATCH_PARENT
         )
-        params.setMargins(10,10,10,10)
+        params.setMargins(10, 10, 10, 10)
 
-        val tr = TableRow(this)
-        tr.layoutParams = params
+        val tr = mutableListOf<TableRow>()
 
-        val o = View.inflate(this,R.layout.tiles_home,null)
-        val o2 = View.inflate(this,R.layout.tiles_home,null)
-        val o3 = View.inflate(this,R.layout.tiles_home,null)
+        val o = TableLayout.inflate(this, R.layout.tiles_home, null)
+        val o2 = TableLayout.inflate(this, R.layout.tiles_home, null)
 
-        tr.addView(o)
-        tr.addView(o2)
+        val x = mutableListOf<TableLayout>()
 
-        table.addView(
-            tr,
-            params
-        )
-        //dynamic field dings fertig
+        var helper = true
 
-//        val intent = Intent(this, writeNote::class.java)
-//        startActivity(intent)
+        for (i in 0..y) {
+            if (helper) {
+                tr.add(TableRow(this))
+                tr[i/2].layoutParams = params
+                x.add(TableLayout.inflate(this, R.layout.tiles_home, null) as TableLayout)
+                tr[i/2].addView(x[i])
+                helper=!helper;
+            }else{
+                x.add(TableLayout.inflate(this, R.layout.tiles_home, null) as TableLayout)
+                tr[i/2].addView(x[i])
+                helper=!helper;
+            }
+        }
+
+        for (trr in tr)
+            table.addView(
+                trr,
+                params
+            )
     }
-
 
 }
